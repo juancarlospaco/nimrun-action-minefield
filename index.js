86d5a31d665342a8bb59ba29f2e8e18a473dcc74
Juan_Carlos <juancarlospaco@gmail.com>
2023-08-29 14:25:46 -0300

    a

--- index.js
+++ index.js
@@ -555,7 +555,7 @@ if (context.eventName === "issue_comment" && (githubComment.startsWith("!nim ") 
       console.log(executeChoosenim("devel"))
       gitInit(context.payload.repository.clone_url)
       setPRBranch(githubClient)
-      console.log(execSync("nim c koch.nim && ./koch boot -d:release", {cwd: gitTempPath}).toString())
+      console.log(execSync(`nim c ${gitTempPath}/koch.nim && ${gitTempPath}/koch boot -d:release`, {cwd: gitTempPath}).toString())
 
     }
   }
